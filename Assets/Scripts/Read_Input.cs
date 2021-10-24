cf5984c4d6da2be431c0d28d520894ee61e0e018
Soondosm <smullaossman@gmail.com>
2021-10-24 04:58:13 -0400

    scrapped WebGL. added loading circle

--- Assets/Scripts/Read_Input.cs
+++ Assets/Scripts/Read_Input.cs
@@ -1,27 +1,31 @@
 using UnityEngine;
 using System.Collections;
-using FrostweepGames.Plugins.Native;
+// using FrostweepGames.Plugins.Native;
 
 public class Read_Input : MonoBehaviour {
  
      AudioClip myAudioClip; 
  
-     void Start() {}
+     void Start() { 
+         GameObject.Find ("Circle").transform.localScale = new Vector3(0, 0, 0); // hide 
+     }
      void Update () {}
      
     async void OnGUI()
     {
          if (GUI.Button(new Rect(10,10,60,50),"Record"))
      { 
-         myAudioClip = CustomMicrophone.Start ( null, false, 10, 44100 );
+         myAudioClip = Microphone.Start ( null, false, 10, 44100 );
         //  myAudioClip = AudioClip.Create("output", 44100, 1, 44100, false);
      }
      if (GUI.Button(new Rect(10,70,60,50),"Send"))
      {
-         SavWav.Save("output", myAudioClip);
-         string input = await TestChunkUpload.Transcribe_Input.Run();
+        SavWav.Save("output", myAudioClip);
+        GameObject.Find ("Circle").transform.localScale = new Vector3(0.5f, 0.5f, 0.5f); // show 
+        string input = await TestChunkUpload.Transcribe_Input.Run();
         Debug.Log(input);
         PlayerMovement.ChangeInput(input);
+        GameObject.Find ("Circle").transform.localScale = new Vector3(0, 0, 0); // hide 
 
  //        audio.Play();
          }
